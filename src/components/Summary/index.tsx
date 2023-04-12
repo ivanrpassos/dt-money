6fc272ddb68f99eb71d36b5e31fb4b1f20cc86ef
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-04-12 19:17:10 -0300

    Aula 13 - Calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -11,30 +11,57 @@ import { TransactionContext } from '../../contexts/TransactionsContext';
 export const Summary = () => {
   const { transactions } = useContext(TransactionContext);
 
+  const summary = transactions.reduce(
+    (
+      // Acumulador
+      acc,
+      transaction,
+    ) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    // Formatando o retorno
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  );
+
   return (
     <SummaryContainer>
+      {/* Entradas */}
       <SummaryCard>
         <header>
           <span>Entradas</span>
           <ArrowCircleUp size={32} color='#00b37e' />
         </header>
-        <strong>R$ 17.500,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
+      {/* Saídas */}
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color='#f75a68' />
         </header>
-        <strong>R$ 17.500,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
+      {/* Total */}
       <SummaryCard variant='green'>
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color='#fff' />
         </header>
-        <strong>R$ 17.500,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
