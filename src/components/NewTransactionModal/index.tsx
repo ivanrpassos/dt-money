6c64285294ed364b832a1e28dc86296bfdaee4a5
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-04-11 23:44:02 -0300

    Aula 9 - Criando radio button acessível

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -43,11 +43,17 @@ export function NewTransactionModal() {
           />
 
           <TransactionType>
-            <TransactionTypeButton variant='income'>
+            <TransactionTypeButton
+              value='income'
+              variant='income'
+            >
               <ArrowCircleUp size={24} />
               Entrada
             </TransactionTypeButton>
-            <TransactionTypeButton variant='outcome'>
+            <TransactionTypeButton
+              value='outcome'
+              variant='outcome'
+            >
               <ArrowCircleDown size={24} />
               Saída
             </TransactionTypeButton>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as RadioGroup from '@radix-ui/react-radio-group';
 
 interface TransactionTypeButtonProps {
   variant: 'income' | 'outcome';
@@ -71,14 +72,16 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${(props) => props.theme['gray-500']};
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
   margin-top: 0.5rem;
 `;
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(
+  RadioGroup.Item,
+)<TransactionTypeButtonProps>`
   background-color: ${(props) => props.theme['gray-700']};
   padding: 1rem;
   display: flex;
@@ -95,5 +98,21 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
       props.variant === 'income'
         ? props.theme['green-300']
         : props.theme['red-300']};
+  }
+
+  &[data-state='checked'] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-500']
+        : props.theme['red-500']};
+    svg {
+      color: ${(props) => props.theme.white};
+    }
+  }
+
+  &[data-state='unchecked']:hover {
+    transition: background-color 0.2s;
+    background: ${(props) => props.theme['gray-600']};
   }
 `;
