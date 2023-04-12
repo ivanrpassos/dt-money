a18ef46ce96f4f7e94749c775c4e76b12ff0eb8d
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-04-11 23:01:54 -0300

    Aula 6 - Criando um modal acessível

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,14 +5,30 @@ import {
 } from './styles';
 import logoImg from '../../assets/logo.svg';
 
+import * as Dialog from '@radix-ui/react-dialog';
+
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt='' />
-        <NewTransactionButton>
-          Nova transação
-        </NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>
+              Nova transação
+            </NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
