6806fe108d95865ee8c843a3381a795fb03229f7
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-04-13 18:22:28 -0300

    Aula 25 - Aplicando o useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,34 +1,37 @@
 import { TransactionContext } from '../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
+import { useMemo } from 'react'
 
 export const useSummary = () => {
   const transactions = useContextSelector(TransactionContext, (context) => {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (
-      // Acumulador
-      acc,
-      transaction,
-    ) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (
+        // Acumulador
+        acc,
+        transaction,
+      ) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
-    // Formatando o retorno
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acc
+      },
+      // Formatando o retorno
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
